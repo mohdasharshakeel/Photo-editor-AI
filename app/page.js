c24878c463146037a17098bed2e8369562cb151a
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-07-22 14:51:40 +0530

    glass-button

--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home(){
   return (
-    <div>
+    <div className="bg-blue-300">
       <h1>Welcome to My App</h1>
       <p>This is the home page.</p>
       <Button variant="primary">hihii</Button>
+      <Button variant="glass">huhuhuh</Button>
     </div>
   );
 }
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -20,10 +20,12 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        primary :"backdrop-blur-lg bg-gradient-to-r from-blue-500 to-purple-600 text-white border-transparent hover:shadow-2xl hover:shadow-blue-500/25 hover:transfrom hover:scale-105 "
+        primary :"backdrop-blur-lg bg-gradient-to-r from-blue-500 to-purple-600 text-white border-transparent hover:shadow-2xl hover:shadow-blue-500/25 hover:transfrom hover:scale-105 ",
+        glass :"backdrop-blur-lg  bg-white/10 text-white border-white/20 hover:bg-white/20 hover:transfrom hover:scale-105 ",
+
       },
       size: { 
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "h-9 px-4 py-2 has-[>svg]:px-3", 
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
