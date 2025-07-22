a2bf7b1875084a34086ca1b6e1be6d63c50065d0
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-07-22 14:47:26 +0530

    button

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home(){
   return (
     <div>
       <h1>Welcome to My App</h1>
       <p>This is the home page.</p>
+      <Button variant="primary">hihii</Button>
     </div>
   );
 }
